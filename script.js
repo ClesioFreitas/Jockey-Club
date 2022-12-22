e65b23391d53f7a0ca8fc7fd57e924de5aa5eb1d
Clesio freitas <clesio.fbarbosa@gmail.com>
2022-12-21 23:15:59 -0300

    function de adicionar apostas pronta

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const CAVALOS = ["Tony", "Val", "Brand", "Twister", "Blue", "Pican"];
 
+let apostas = [];
+
 function adicionarApostas(){
   const inCavalo = document.querySelector("#inCavalo")
   const inValor = document.querySelector("#inValor")
@@ -8,12 +10,36 @@ function adicionarApostas(){
   const cavalo = Number(inCavalo.value);
   const valor = Number(inValor.value);
 
-  if(isNaN(cavalo) || isNaN(valor) || valor === 0 || validaApostar(cavalo)){
-    alert("Cavalo inválido...")
+  if(isNaN(cavalo) || isNaN(valor) || !validaApostar(cavalo)){
+    alert("Cavalo inválido...");
     return;
   }
 
+  apostas.push({cavalo: cavalo, valor: valor})
+
+  let lista = "Apostas Realizadas\n---------------------------\n"
+
+  for(let i = 0; i < apostas.length; i++){
+    lista += "N " + apostas[i].cavalo + " " + obterCavalo(apostas[i].cavalo);
+    lista += " - R$: " + apostas[i].valor.toFixed(2) + "\n";
+  }
+
+  outApostas.textContent = lista
 }
 
 const btApostar = document.querySelector("#btApostar");
 btApostar.addEventListener("click", adicionarApostas)
+
+function validaApostar(num){
+  const tam = CAVALOS.length;
+  if(num >= 1 && num <= tam){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function obterCavalo(numCavalo){
+  const posicao = [numCavalo - 1]
+  return CAVALOS[posicao]
+}
